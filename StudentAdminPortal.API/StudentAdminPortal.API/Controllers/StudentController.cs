ce700a9a42e1209305e0d1c05a1ccfdaf0c5fc71
AryaKumari4196 <kumariaryajha@gmail.com>
2023-06-26 04:06:03 +0530

    2nd Commit

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        [Route("[controller]")]
+        public async  Task<IActionResult> GetAllStudents()
+        {
+          var students= await studentRepository.GetStudentsAsync();
+            mapper.Map<List<Student>>(students);
+      /*      var domainModelStudents = new List<Student>() ;
+            foreach( var student in students)
+            {
+                domainModelStudents.Add(new Student()
+                {
+                    Id = student.Id,
+                    FirstName = student.FirstName,
+                    LastName = student.LastName,
+                    Email = student.Email,
+                    DateOfBirth = student.DateOfBirth,
+                    Mobile = student.Mobile,
+                    ProfileImageUrl = student.ProfileImageUrl,
+                    GenderId = student.GenderId,
+                    Address= new Address()
+                    {
+                        Id=student.Address.Id,
+                        PhysicalAddress=student.Address.PhysicalAddress,
+                        PostalAddress=student.Address.PostalAddress
+                    },
+                    Gender = new Gender()
+                    {
+                        Id= student.Gender.Id,
+                        Description=student.Gender.Description
+
+                    }
+                });
+            }*/
+            return Ok();
+        }
+
+    }
+}
--- StudentAdminPortal.API/StudentAdminPortal.API/Startup.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Startup.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+using StudentAdminPortal.API.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,14 +30,27 @@ namespace StudentAdminPortal.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //  services.AddDbContext<StudentAdminContext>;
+            services.AddDbContext<StudentAdminContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("StudentAdminPortal")));
+            services.AddScoped<IStudentRepository, SqlStudentRepository>();
+            services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "StudentAdminPortal. API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+ 
+
             }
 
             app.UseHttpsRedirection();
